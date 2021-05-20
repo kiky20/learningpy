92c3fe22b70bd85cbd8ff0112a858f5f139ee77e
Rizky Wira Pratama <kikywira20@gmail.com>
2021-05-20 20:24:42 +0800

    Add files via upload
    this is my basic loop learning file

---
+++ loop-basic.py.py
@@ -0,0 +1,20 @@
+#this is a for loop statement
+for v in range(6):
+  print(v)
+#the v is just a random variable,it will s
+
+#in below is a "while loops" example
+#start by declaring a variabel(or you can use exsited variabel)
+x = 1
+#stating the condition first
+while x <= 5:
+    print(x)
+    #dont forget the order too like this one. or it will looping endlessly
+    x += 1
+#and this one is nested loop
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
