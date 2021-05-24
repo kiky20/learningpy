52963451d23050fd663b057457f6e04411cc1f27
Rizky Wira Pratama <kikywira20@gmail.com>
2021-05-24 23:43:39 +0800

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,5 @@
+#there are 4 basic data type in python
+int = 12 #integer to storage fixed numerical value
+string = "str" #string to store phrases
+float = 12.3 #float to store decimals
+bool_ = True #boolean to store statement like true or false
