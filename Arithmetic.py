504ada021043a9d0b14b51f3f0a1a068db312382
Rizky Wira Pratama <kikywira20@gmail.com>
2021-05-26 23:41:44 +0800

    basic

---
+++ Arithmetic.py
@@ -0,0 +1,31 @@
+#basic arithemtics in python
+
+# (+) (-) (* or **) and divisions (/ or // or %)
+print(2 + 2)
+print(2 - 1)
+
+#for multiply
+
+print(2*3) #this are normal multiplication
+print(2**3) #this are for multiplication power
+
+#for divisions
+print(2/2) #the divisions that print integer (int)
+print(2//2)#the divisions that print float (float)
+print(2%2)#the divisions that print remaining value
+
+#Comparison Operator
+
+s = 3>2
+print(s) #this will print a boolean value
+
+#these are the comparison
+#>,<,>=,<=,==,!+
+
+#augmented
+x = 10
+
+x += 3
+#or
+x = x+3
+print(x) 
