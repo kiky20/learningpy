bae5cfe3f37f017a631e58897ae85541bae17b21
Rizky Wira Pratama <kikywira20@gmail.com>
2021-05-28 23:39:54 +0800

    Update datatype.py

--- datatype.py
+++ datatype.py
@@ -3,3 +3,6 @@ int = 12 #integer to storage fixed numerical value
 string = "str" #string to store phrases
 float = 12.3 #float to store decimals
 bool_ = True #boolean to store statement like true or false
+
+#you can change data type to when needed
+str(int)#this row to convert the variable to a string value
